20c038d77e066d74ed56f3a260b407a170415800
DanOwen91 <danowen1@outlook.com>
2019-01-09 19:26:57 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alogrithmns_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
